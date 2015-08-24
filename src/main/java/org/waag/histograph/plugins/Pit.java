8b9dd33a99bccbfbaf219617480bb3e0ddfe0b99
Bert Spaan <bert.spaan@gmail.com>
2015-08-24 11:38:05 +0200

    expect date fields to be array

--- src/main/java/org/waag/histograph/plugins/Pit.java
+++ src/main/java/org/waag/histograph/plugins/Pit.java
@@ -16,7 +16,7 @@ public class Pit {
 
   public static final Set<String> PROPERTY_KEYS_EXCLUDE = new HashSet<String>(Arrays.asList(new String[]{"created", "accessTime", "counter"}));
   public static final Set<String> PROPERTY_KEYS_SIMPLE = new HashSet<String>(Arrays.asList(new String[]{"id", "uri", "name", "type"}));
-  public static final Set<String> PROPERTY_KEYS_RAW = new HashSet<String>(Arrays.asList(new String[]{"geometry", "data", "validSince", "validUntil"}));
+  public static final Set<String> PROPERTY_KEYS_RAW = new HashSet<String>(Arrays.asList(new String[]{"geometry", "data"}));
 
   public Pit(Node pit) {
     this.pit = pit;
